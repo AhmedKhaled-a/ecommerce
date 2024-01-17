3f28bc00973e27f0077f5422dfe95599f20e1a71
AhmedKhaled <ahmedfarfouroo7@gmail.com>
2024-01-17 22:02:49 +0200

    changing categories page layout

--- js/category.js
+++ js/category.js
@@ -1,20 +1,31 @@
 let productsContainer = document.querySelector('.p-container');
-let priceRange = document.querySelector('input[type=range]');
-let price =  priceRange.value;
-let maxPrice =  priceRange.getAttribute('max');
+let minPrice =  document.querySelector('.range-min');
+let maxPrice =  document.querySelector('.range-max');
+let all = document.querySelector('.all');
 let Electronics = document.querySelector('.electronics');
 let Jewelery = document.querySelector('.jewelery');
 let Men =document.querySelector('.men');
 let Women= document.querySelector('.women');
 let search=document.querySelector('.search');
-let categoryCounter = document.querySelector('.catCounter');
-getAllProducts(price,maxPrice);
+getAllProducts(minPrice.value,maxPrice.value);
 
-priceRange.addEventListener('change',()=>{
-    price =  priceRange.value;
-    maxPrice =  priceRange.getAttribute('max');
-    getAllProducts(price,maxPrice)
+minPrice.addEventListener('change',()=>{
+    console.log("changing");
+    min =  minPrice.value;
+    max =  maxPrice.value;
+    getAllProducts(min,max)
 })
+maxPrice.addEventListener('change',()=>{
+    console.log("changing");
+    min =  minPrice.value;
+    max =  maxPrice.value;
+    getAllProducts(min,max)
+})
+
+all.addEventListener('click',()=>{
+    getAllProducts(minPrice.value,maxPrice.value);
+})
+
 Electronics.addEventListener('click',()=>{
     getCategory ('electronics');
 })
@@ -31,25 +42,27 @@ Women.addEventListener('click',()=>{
     getCategory("women's clothing");
 })
 
-async function getAllProducts (price,maxPrice) {
+async function getAllProducts (minPrice,maxPrice) {
     var apiresponse = await fetch('https://fakestoreapi.com/products')
     var finalResult = await apiresponse.json()
     // console.log(finalResult[0]);
     let box = '';
     for (const i of finalResult) {
-        if(i.price >= price && i.price < maxPrice)
+        if(i.price >= minPrice && i.price < maxPrice)
         {
             box += `
-            <div class="product-card" id='${i.id}'>
-                <img src="${i.image}">
-                <div>
-                    <h2 class="product-name">${i.title}</h2>
-                    <p class="product-category">${i.category}</p>
-                    <p class="price">${i.price}</p>
-                    <p class="product-stars">${i.rating.rate}</p>
-                </div>
-                <button class="addToCart"><i class="fa-solid fa-bag-shopping"></i></button>
-          </div>
+          <div class="col-md-3  p-0 mx-3 rounded product-card" id='${i.id}'>
+                        <div class="product-img">
+                            <img src="${i.image}" alt="" class="w-100">
+                        </div>
+                        <div class="p-2">
+                            <h5 class="product-name long-text">${i.title}</h5>
+                            <p class="product-category text-muted">${i.category}</p>
+                            <p class="price fw-semibold">$${i.price}</p>
+                            <p class="product-stars">${i.rating.rate}</p>
+                        </div>
+                        <button class="addToCart"><i class="fa-solid fa-bag-shopping"></i></button>
+            </div>
             `
         }
     }
@@ -64,16 +77,18 @@ async function getCategory (category) {
     let box = '';
     for (const i of finalResult) {
         box += `
-        <div class="product-card" id='${i.id}'>
-            <img src="${i.image}">
-            <div>
-                <h2 class="product-name">${i.title}</h2>
-                <p class="product-category">${i.category}</p>
-                <p class="price">${i.price}</p>
-                <p class="product-stars">${i.rating.rate}</p>
+        <div class="col-md-3  p-0 mx-3 rounded product-card" id='${i.id}'>
+                        <div class="product-img">
+                            <img src="${i.image}" alt="" class="w-100">
+                        </div>
+                        <div class="p-2">
+                            <h5 class="product-name long-text">${i.title}</h5>
+                            <p class="product-category text-muted">${i.category}</p>
+                            <p class="price fw-semibold">$${i.price}</p>
+                            <p class="product-stars">${i.rating.rate}</p>
+                        </div>
+                        <button class="addToCart"><i class="fa-solid fa-bag-shopping"></i></button>
             </div>
-            <button class="addToCart"><i class="fa-solid fa-bag-shopping"></i></button>
-        </div>
             `
     }
     productsContainer.innerHTML=box;
@@ -91,16 +106,19 @@ async function searchProduct (match) {
 
         
         box += `
-        <div class="product-card" id='${i.id}'>
-            <img src="${i.image}">
-            <div>
-                <h2 class="product-name">${i.title}</h2>
-                <p class="product-category">${i.category}</p>
-                <p class="price">${i.price}</p>
-                <p class="product-stars">${i.rating.rate}</p>
-            </div>
-            <button class="addToCart"><i class="fa-solid fa-bag-shopping"></i></button>
-        </div> `
+        <div class="col-md-3  p-0 mx-3 rounded product-card" id='${i.id}'>
+        <div class="product-img">
+            <img src="${i.image}" alt="" class="w-100">
+        </div>
+        <div class="p-2">
+            <h5 class="product-name">${i.title}</h5>
+            <p class="product-category text-muted">${i.category}</p>
+            <p class="price fw-semibold">$${i.price}</p>
+            <p class="product-stars">${i.rating.rate}</p>
+        </div>
+        <button class="addToCart"><i class="fa-solid fa-bag-shopping"></i></button>
+        </div> 
+        `
     }
     productsContainer.innerHTML=box;
 }}
--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
-
-
+// SIDEBAR SCRIPT
 const sidebar = document.querySelector('.sidebar'),
     toggle = document.querySelector('.toggle-sidebar'),
     toggle2 = document.querySelector('#cart-slider-toggle');
@@ -10,4 +9,54 @@ toggle.addEventListener('click', () => {
 
 toggle2.addEventListener('click', (e) => {
     sidebar.classList.toggle('closed')
+})
+
+// PRICE RANGE SCRIPT
+const rangeInput = document.querySelectorAll('.range-input input'),
+    priceInput = document.querySelectorAll('.price-input input')
+progress = document.querySelector('.slider .slider-progress');
+let priceGap = 100;
+
+
+rangeInput.forEach(input => {
+    input.addEventListener('input', e => {
+        // getting two ranges values and parsing them into numbers
+        let minVal = parseInt(rangeInput[0].value),
+            maxVal = parseInt(rangeInput[1].value);
+
+
+
+        if (maxVal - minVal < priceGap) {
+            if (e.target.className === "range-min") { // if active slider is min slider
+                rangeInput[0].value = maxVal - priceGap;
+            } else {
+                rangeInput[1].value = minVal + priceGap;
+            }
+
+        } else {
+            priceInput[0].value = minVal;
+            priceInput[1].value = maxVal;
+            progress.style.left = (minVal / rangeInput[0].max) * 100 + "%";
+            progress.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+        }
+    })
+})
+
+
+priceInput.forEach(input => {
+    input.addEventListener('input', e => {
+        // getting two inputs values and parsing them into numbers
+        let minVal = parseInt(priceInput[0].value),
+            maxVal = parseInt(priceInput[1].value);
+
+        if ((maxVal - minVal >= priceGap) && maxVal <= rangeInput[1].max && minVal >= rangeInput[0].min) {
+            if (e.target.className === "input-min") { // if active input is min input
+                rangeInput[0].value = minVal;
+                progress.style.left = (minVal / rangeInput[0].max) * 100 + "%";
+            }else {
+                rangeInput[1].value = maxVal;
+                progress.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+            }
+        } 
+    })
 })
