b02f264dde3737183808e85e1c1cff009b407e6b
Ahmad-Mohamad-Ibrahim <ahmadmohamadibrahim11@gmail.com>
2024-01-17 13:29:13 +0200

    Adding Checkout Page

---
+++ js/cart-checkout.js
@@ -0,0 +1,79 @@
+let userCart = [];
+
+
+
+document.getElementById("checkOutForm").addEventListener("submit" , (e) => {
+    e.preventDefault();
+});
+
+function checkOut() {
+    let usrID = getLoggedInUser();
+    // alert
+    alert("You Checked Out");
+    // empty cart
+    userCart.every((userProducts) => {
+        if(Number(userProducts.userId) == usrID) {
+            userProducts.products = []; // empty cart
+            return false ;
+        }
+        else {
+            return true;
+        }
+
+    });
+
+    setLocalStr();
+
+
+
+    // go to index
+    window.location.replace("./index.html");
+}
+
+function renderCheckoutProducts() {
+    let usrID = getLoggedInUser();
+    let totalCalculated = document.getElementById("totalCal");
+    let numberOfProductsEl = document.getElementById("productNo");
+    let numberOfProducts = 0;
+    let checkoutCartHeader = document.getElementById("checkoutCartHeader");
+
+    if(userCart != null) {
+        userCart.forEach((userProducts) => {
+            // console.log(typeof(userProducts));
+            let total = 0;
+            let productParEl;
+            let productNameEl;
+            let productPriceEl;
+            let price = 0;
+            if(Number(userProducts.userId) == usrID) {
+                userProducts.products.forEach((productItem) => {
+                    // console.log(productItem);
+                    price = productItem.productPrice;
+                    numberOfProducts++;
+                    total += price * productItem.productQuantity;
+                    productParEl = document.createElement("p");
+                    productNameEl = document.createElement("span");
+                    productNameEl.innerHTML = productItem.productName + " ........... ";
+                    productPriceEl = document.createElement("span");
+                    productPriceEl.innerHTML = price * productItem.productQuantity ;
+
+                    productParEl.insertAdjacentElement("beforeend" , productNameEl);
+                    productParEl.insertAdjacentElement("beforeend" , productPriceEl);
+
+                    checkoutCartHeader.insertAdjacentElement("afterend", productParEl);
+
+                });
+                totalCalculated.innerText = total;
+                numberOfProductsEl.innerText = numberOfProducts;
+                // console.log(total);
+            }
+            return;
+        });
+    }
+}
+
+getFromLocalStorage();
+renderCheckoutProducts();
+
+
+
---
+++ js/cart-helper.js
@@ -0,0 +1,101 @@
+
+function getFromLocalStorage() {
+    userCart = JSON.parse(localStorage.getItem("userCart"));
+    // console.log(userCart);
+}
+
+function updateProduct(e, usrID) {
+    let val = e.target.value;
+    let productRow = e.target.parentNode.parentNode;
+    let productId = productRow.getAttribute("id");
+    // console.log(productId);
+    let currentSubTotal = e.target.parentNode.nextElementSibling;
+    // let allSubTotals = document.querySelectorAll(".subtotal");
+    let price = 0;
+    // console.log(currentSubTotal);
+
+
+    // search through userCart with productId
+    userCart.every((userProducts) => {
+        if(Number(userProducts.userId) == usrID) {
+            // update value
+
+            userProducts.products.every((product) => {
+                console.log(product);
+                if(productId == product.productId) {
+                    product.productQuantity = val;
+                    price = product.productPrice;
+                    // console.log(price);
+                    return false; // break
+                }
+                else {
+                    return true; // continue
+                }
+            });
+            return false; // break
+        }
+    });
+    // set local storage
+    setLocalStr();
+
+    // update subtotal and total
+
+    // round to two decimal points
+    currentSubTotal.innerHTML = (val * price).toFixed(2);;
+    updateTotalPrice();
+}
+
+function removeProduct(e, usrID) {
+    // console.log("here");
+    let productRow = e.target.parentNode.parentNode;
+    // console.log(productRow);
+    let productId = productRow.getAttribute("id");
+    // remove
+    userCart.every((userProducts) => {
+        if(Number(userProducts.userId) == usrID) {
+            let n = userProducts.products.length;
+            for(let i = 0; i < n ; i++ ) {
+                let currentProduct = userProducts.products[i];
+                if(productId == currentProduct.productId) {
+                    productRow.remove();
+                    userProducts.products.splice(i, 1);
+                    break;
+                }
+            }
+
+            console.log(userCart);
+            
+            setLocalStr();
+            
+        }
+    });
+
+    updateTotalPrice();
+
+    
+}
+
+function updateTotalPrice() {
+    let allSubTotals = document.querySelectorAll(".subtotal");
+    let totalEle = document.querySelector("#totalCalc");
+    let total = 0;
+
+    allSubTotals.forEach((subTotalE) => {
+        // console.log(subTotalE);
+        total += Number(subTotalE.innerText);
+    });
+
+    totalEle.innerHTML = total;
+
+}
+
+
+function setLocalStr() {
+    localStorage.setItem("userCart" , JSON.stringify(userCart));
+}
+
+function getLoggedInUser() {
+    let userId = JSON.parse(localStorage.getItem("loggedInUser")).userId;
+    return userId;
+
+}
