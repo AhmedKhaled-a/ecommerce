190b394e3b2d857ec6325666c41ce121d6db9a8e
AhmedKhaled <ahmedfarfouroo7@gmail.com>
2024-01-18 18:19:34 +0200

    gold star

--- js/script.js
+++ js/script.js
@@ -23,16 +23,12 @@ rangeInput.forEach(input => {
         // getting two ranges values and parsing them into numbers
         let minVal = parseInt(rangeInput[0].value),
             maxVal = parseInt(rangeInput[1].value);
-
-
-
         if (maxVal - minVal < priceGap) {
             if (e.target.className === "range-min") { // if active slider is min slider
                 rangeInput[0].value = maxVal - priceGap;
             } else {
                 rangeInput[1].value = minVal + priceGap;
             }
-
         } else {
             priceInput[0].value = minVal;
             priceInput[1].value = maxVal;
